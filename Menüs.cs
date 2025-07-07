ed8f9e886e5dbed691f9cd8d89e1f44d6175b49a
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2025-07-07 23:02:50 +0200

    update

--- Menüs.cs
+++ Menüs.cs
@@ -12,7 +12,7 @@ namespace Smake.io
         public static int randzahl;
 
         //Freischaltbare Skins/Farbenf
-        public static ConsoleColor[] farben = [
+        public readonly static ConsoleColor[] farben = [
                             ConsoleColor.White,
                             ConsoleColor.Blue,
                             ConsoleColor.DarkBlue,
@@ -29,9 +29,9 @@ namespace Smake.io
                             ConsoleColor.Gray,
                             ConsoleColor.DarkGray,
                                 ];
-        public static char[] tailskins = ['+', 'x', '~', '=', '-', 'o', '•'];
-        public static char[] foodskins = ['*', '@', '$', '♥', '%', '¤', '&'];
-        public static char[] randskins = ['█', '#', '▓', '░', '■', '▌', '▒'];
+        public readonly static char[] tailskins = ['+', 'x', '~', '=', '-', 'o', '•'];
+        public readonly static char[] foodskins = ['*', '@', '$', '♥', '%', '¤', '&'];
+        public readonly static char[] randskins = ['█', '#', '▓', '░', '■', '▌', '▒'];
 
         //Freigeschalteneskins/farben
         public static bool[] freigeschaltetTail = new bool[tailskins.Length];
@@ -41,16 +41,16 @@ namespace Smake.io
 
         //Preise Skin/Farben
 
-        static int[] TailPreis = [300, 350, 400, 450, 500];
-        static int[] FoodPreis = [350, 500, 400, 300, 250, 250];
-        static int[] RandPreis = [200, 400, 400, 350, 300, 400];
-        static int[] FarbenPreis = [100, 259, 100, 250, 200, 300, 100, 300, 175, 250, 100, 250, 450, 500];
+        public static readonly int[] TailPreis = [300, 350, 400, 450, 500];
+        public static readonly int[] FoodPreis = [350, 500, 400, 300, 250, 250];
+        public static readonly int[] RandPreis = [200, 400, 400, 350, 300, 400];
+        public static readonly int[] FarbenPreis = [100, 259, 100, 250, 200, 300, 100, 300, 175, 250, 100, 250, 450, 500];
 
         // Level für Skin/Farben
-        static int[] TailLevel = [2, 4, 6, 8, 10];
-        static int[] FoodLevel = [2, 4, 6, 8, 10, 12, 14];
-        static int[] RandLevel = [2, 4, 6, 8, 10, 12, 14];
-        static int[] FarbenLevel = [0, 4, 0, 6, 8, 10, 0, 10, 20, 25, 0, 20, 25, 30];
+        public static readonly int[] TailLevel = [2, 4, 6, 8, 10];
+        public static readonly int[] FoodLevel = [2, 4, 6, 8, 10, 12, 14];
+        public static readonly int[] RandLevel = [2, 4, 6, 8, 10, 12, 14];
+        public static readonly int[] FarbenLevel = [0, 4, 0, 6, 8, 10, 0, 10, 20, 25, 0, 20, 25, 30];
 
         // Eingaben für Spielernamen
         public static void Eingaben()
@@ -92,14 +92,14 @@ namespace Smake.io
             }
 
             Console.Clear();
-            DrawTitle();
+            MenüRenderer.DrawTitle();
             bool menu = true;
             int MenueOptions = 1;
 
             do
             {
                 Console.SetCursorPosition(0, 11);
-                ShowMenuOptions(MenueOptions);
+                MenüRenderer.RenderMainMenueOptions(MenueOptions);
 
                 while (Console.KeyAvailable)
                     Console.ReadKey(true);
@@ -141,10 +141,10 @@ namespace Smake.io
                     Skin_Farben();
                     break;
                 case 6:
-                    Anleitung();
+                    MenüRenderer.RenderAnleitung();
                     break;
                 case 5:
-                    Statistiken();
+                    MenüRenderer.RenderStatistiken();
                     break;
                 case 7:
                     Spiellogik.exit = true;
@@ -152,52 +152,6 @@ namespace Smake.io
             }
         }
 
-        // Titelbild
-        static void DrawTitle()
-        {
-            Console.SetCursorPosition(0, 0);
-            Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine(@"
-  ██████  ███▄ ▄███▓ ▄▄▄       ██ ▄█▀▓█████ 
-▒██    ▒ ▓██▒▀█▀ ██▒▒████▄     ██▄█▒ ▓█   ▀ 
-░ ▓██▄   ▓██    ▓██░▒██  ▀█▄  ▓███▄░ ▒███   
-  ▒   ██▒▒██    ▒██ ░██▄▄▄▄██ ▓██ █▄ ▒▓█  ▄ 
-▒██████▒▒▒██▒   ░██▒ ▓█   ▓██▒▒██▒ █▄░▒████▒
-▒ ▒▓▒ ▒ ░░ ▒░   ░  ░ ▒▒   ▓▒█░▒ ▒▒ ▓▒░░ ▒░ ░
-░ ░▒  ░ ░░  ░      ░  ▒   ▒▒ ░░ ░▒ ▒░ ░ ░  ░
-░  ░  ░  ░      ░     ░   ▒   ░ ░░ ░    ░   
-      ░         ░         ░  ░░  ░      ░  ░
-");
-            Console.ResetColor();
-        }
-
-        // Auswahlmöglichkeiten im Hauptmenü
-        static void ShowMenuOptions(int selected)
-        {
-            Console.WriteLine("╔══════════════════════════════╗");
-            Console.WriteLine("║       SMAKE MAIN MENU        ║");
-            Console.WriteLine("╠══════════════════════════════╣");
-
-            for (int i = 0; i < 7; i++)
-            {
-
-                string option = i switch
-                {
-                    0 => "Spiel starten",
-                    1 => "Einstellungen",
-                    2 => "Shop",
-                    3 => "Skins/Farben",
-                    4 => "Statistiken",
-                    5 => "Anleitung",
-                    6 => "Beenden",
-                    _ => ""
-                };
-                string zeiger = (i + 1 == selected) ? ">>" : "  ";
-                Console.WriteLine($"║  {zeiger} {option,-25}║");
-            }
-
-            Console.WriteLine("╚══════════════════════════════╝");
-        }
         // Einstellungsmenü im Hauptmenü
         static void Einstellungen()
         {
@@ -207,7 +161,7 @@ namespace Smake.io
 
             do
             {
-                EinstellungenOptions(einstellungsAuswahl);
+                MenüRenderer.RenderEinstellungen(einstellungsAuswahl);
 
                 while (Console.KeyAvailable)
                     Console.ReadKey(true);
@@ -261,34 +215,6 @@ namespace Smake.io
             } while (menu);
         }
 
-        // Einstellmöglichkeiten im Einstellungsmenü
-        static void EinstellungenOptions(int selected)
-        {
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("EINSTELLUNGEN");
-            Console.WriteLine("══════════════════════════════");
-
-
-            for (int i = 0; i < 7; i++)
-            {
-                string option = i switch
-                {
-                    0 => $"Schwierigkeit ändern   [Aktuell: {Spiellogik.difficulty}]",
-                    1 => $"Multiplayer            [Aktuell: {(Spiellogik.multiplayer ? "An" : "Aus")}]",
-                    2 => $"Gamemode ändern        [Aktuell: {Spiellogik.gamemode}]",
-                    3 => $"Performance mode       [Aktuell: {(RendernSpielfeld.performancemode ? "An" : "Aus")}]",
-                    4 => $"Musik AN/AUS           [Aktuell: {(Musik.musikplay ? "An" : "Aus")}]",
-                    5 => $"Sounds AN/AUS          [Aktuell: {(Musik.soundplay ? "An" : "Aus")}]",
-                    6 => "Zurück zum Hauptmenü",
-                    _ => ""
-                };
-                string zeiger = (i + 1 == selected) ? ">>" : "  ";
-                Console.WriteLine($"{zeiger} {option,-25}");
-            }
-
-            Console.WriteLine("══════════════════════════════");
-        }
-
         // Auswahl der Spielgeschwindigkeit
         static void ChangeDifficulty()
         {
@@ -324,7 +250,7 @@ namespace Smake.io
                     if (auswahl < 1) auswahl = gesamtOptionenSkins;
                     if (auswahl > gesamtOptionenSkins) auswahl = 1;
 
-                    ShopSkinsOptions(auswahl);
+                    MenüRenderer.RenderShopSkinsOptions(auswahl);
 
                     while (Console.KeyAvailable)
                         Console.ReadKey(true);
@@ -391,7 +317,7 @@ namespace Smake.io
                     if (auswahl < 1) auswahl = gesamtOptionenFarben;
                     if (auswahl > gesamtOptionenFarben) auswahl = 1;
 
-                    ShopFarbenOptions(auswahl);
+                    MenüRenderer.RenderShopFarbenOptions(auswahl);
 
                     while (Console.KeyAvailable)
                         Console.ReadKey(true);
@@ -435,110 +361,6 @@ namespace Smake.io
             } while (menu);
         }
 
-        // Zeigt die Farbenseite vom Shop
-        static void ShopFarbenOptions(int selected)
-        {
-
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("           Shop           ");
-            Console.WriteLine($"Coins: {Program.coins}");
-            Console.WriteLine($"Level: {Program.level}");
-            Console.WriteLine("═══════════════════════════");
-            Console.WriteLine("←  Wechsle die Shopseite  →");
-
-            int option = 0;
-
-            Console.WriteLine("\nFarben:");
-            for (int i = 1; i < farben.Length; i++, option++)
-            {
-                string shoptext;
-
-                if (Program.level < FarbenLevel[i - 1])
-                {
-                    shoptext = $"[Benötigtes Level: {FarbenLevel[i - 1]}]";
-                }
-                else
-                {
-                    shoptext = freigeschaltetFarben[i] ? "[Freigeschaltet]" : "[" + FarbenPreis[i - 1] + " Coins]";
-                }
-                string zeiger = (option + 1 == selected) ? ">>" : "  ";
-                Console.ForegroundColor = farben[i];
-                Console.WriteLine($"{zeiger} {farben[i],-12} {shoptext}");
-                Console.ResetColor();
-            }
-
-            string zeiger2 = (option + 1 == selected) ? ">>" : "  ";
-            Console.WriteLine($"\n{zeiger2} Zurück zum Hauptmenü");
-            Console.WriteLine("══════════════════════════");
-        }
-
-        // Zeigt die Skinseite vom Shop
-        static void ShopSkinsOptions(int selected)
-        {
-            Console.SetCursorPosition(0, 0);
-            Console.WriteLine("           Shop           ");
-            Console.WriteLine($"Coins: {Program.coins}");
-            Console.WriteLine($"Level: {Program.level}");
-            Console.WriteLine("═══════════════════════════");
-            Console.WriteLine("←  Wechsle die Shopseite  →");
-            int option = 0;
-
-            Console.WriteLine("\nTail Skins:");
-            for (int i = 2; i < tailskins.Length; i++, option++)
-            {
-                string shoptext;
-
-                if (Program.level < TailLevel[i - 2])
-                {
-                    shoptext = $"[Benötigtes Level: {TailLevel[i - 2]}]";
-                }
-                else
-                {
-                    shoptext = freigeschaltetTail[i] ? "[Freigeschaltet]" : "[" + TailPreis[i - 2] + " Coins]";
-                }
-                string zeiger = (option + 1 == selected) ? ">>" : "  ";
-                Console.WriteLine($"{zeiger} {tailskins[i]} {shoptext}");
-            }
-
-            Console.WriteLine("\nFood Skins:");
-            for (int i = 1; i < foodskins.Length; i++, option++)
-            {
-                string shoptext;
-
-                if (Program.level < FoodLevel[i - 1])
-                {
-                    shoptext = $"[Benötigtes Level: {FoodLevel[i - 1]}]";
-                }
-                else
-                {
-                    shoptext = freigeschaltetFood[i] ? "[Freigeschaltet]" : "[" + FoodPreis[i - 1] + " Coins]";
-                }
-                string zeiger = (option + 1 == selected) ? ">>" : "  ";
-                Console.WriteLine($"{zeiger} {foodskins[i]} {shoptext}");
-            }
-
-            Console.WriteLine("\nRand Skins:");
-            for (int i = 1; i < randskins.Length; i++, option++)
-            {
-                string shoptext;
-
-                if (Program.level < RandLevel[i - 1])
-                {
-                    shoptext = $"[Benötigtes Level: {RandLevel[i - 1]}";
-                }
-                else
-                {
-                    shoptext = freigeschaltetRand[i] ? "[Freigeschaltet]" : "[" + RandPreis[i - 1] + " Coins]";
-                }
-                string zeiger = (option + 1 == selected) ? ">>" : "  ";
-                Console.WriteLine($"{zeiger} {randskins[i]} {shoptext}");
-            }
-
-            string zeiger2 = (option + 1 == selected) ? ">>" : "  ";
-            Console.WriteLine($"\n{zeiger2} Zurück zum Hauptmenü");
-            Console.WriteLine("══════════════════════════");
-        }
-
         // Logik für Skin und Farben menü
         static void Skin_Farben()
         {
@@ -548,7 +370,7 @@ namespace Smake.io
 
             do
             {
-                Skin_FarbenOptions(Skin_FarbenAuswahl);
+                MenüRenderer.RenderSkin_FarbenOptions(Skin_FarbenAuswahl);
 
                 while (Console.KeyAvailable)
                     Console.ReadKey(true);
@@ -675,164 +497,6 @@ namespace Smake.io
 
             }
             while (menu);
-        }
-
-        // Auswahlmenü für Skins und Farben im Hauptmenü
-        static void Skin_FarbenOptions(int selected)
-        {
-            Console.SetCursorPosition(0, 0);
-            Console.ResetColor();
-            Console.WriteLine("Skins/Farben");
-            Console.WriteLine("══════════════════════════════");
-
-            bool color = false;
-
-            for (int i = 0; i < 11; i++)
-            {
-                ConsoleColor farbemenue = ConsoleColor.White;
-                string option = "";
-
-                switch (i)
-                {
-                    case 0:
-                        option = $"Player 1 Tailskin ändern    [Aktuell: {Spiellogik.player.Skin}]";
-                        color = false;
-                        break;
-                    case 1:
-                        option = $"Player 2 Tailskin ändern    [Aktuell: {Spiellogik.player2.Skin}]";
-                        color = false;
-                        break;
-                    case 2:
-                        option = $"Foodskin ändern             [Aktuell: {Spiellogik.food}]";
-                        color = false;
-                        break;
-                    case 3:
-                        option = $"Randskin ändern             [Aktuell: {Spiellogik.rand}]";
-                        color = false;
-                        break;
-                    case 4:
-                        option = $"Player 1 Farbe ändern       [Aktuell: ";
-                        farbemenue = Spiellogik.player.Headfarbe;
-                        color = true;
-                        break;
-                    case 5:
-                        option = $"Player 2 Farbe ändern       [Aktuell: ";
-                        farbemenue = Spiellogik.player2.Headfarbe;
-                        color = true;
-                        break;
-                    case 6:
-                        option = $"Player 1 Tailfarbe ändern   [Aktuell: ";
-                        farbemenue = Spiellogik.player.Farbe;
-                        color = true;
-                        break;
-                    case 7:
-                        option = $"Player 2 Tailfarbe ändern   [Aktuell: ";
-                        farbemenue = Spiellogik.player2.Farbe;
-                        color = true;
-                        break;
-
-                    case 8:
-                        option = $"Foodfarbe ändern            [Aktuell: ";
-                        farbemenue = Spiellogik.foodfarbe;
-                        color = true;
-                        break;
-
-                    case 9:
-                        option = $"Randfarbe ändern            [Aktuell: ";
-                        farbemenue = Spiellogik.randfarbe;
-                        color = true;
-                        break;
-                    case 10:
-                        color = false;
-                        option = "Zurück zum Hauptmenü";
-                        break;
-                }
-                Console.ResetColor();
-                string zeiger = (i + 1 == selected) ? ">>" : "  ";
-                if (color)
-                {
-                    Console.Write(zeiger + " " + option);
-                    if (RendernSpielfeld.performancemode)
-                    {
-
-                        Console.Write("Performance Mode AN");
-                        Console.WriteLine("]");
-
-                    }
-                    else
-                    {
-                        Console.ForegroundColor = farbemenue;
-                        Console.Write(farbemenue);
-                        Console.ResetColor();
-                        Console.WriteLine("]");
-                    }
-                }
-                else
-                {
-                    Console.WriteLine(zeiger + " " + option);
-                }
-            }
-            Console.WriteLine("══════════════════════════════");
-        }
-
-        // Anleitung im Hauptmenü
-        static void Anleitung()
-        {
-            Console.Clear();
-            Console.WriteLine("ANLEITUNG");
-            Console.WriteLine("══════════════════════════════");
-            Console.WriteLine($"Ziel: Iss so viele {Spiellogik.food} wie möglich!");
-            Console.WriteLine();
-            Console.WriteLine("Steuerung:");
-            Console.WriteLine();
-            Console.WriteLine("Spieler 1:");
-            Console.WriteLine("  ↑ - Hoch");
-            Console.WriteLine("  ← - Links");
-            Console.WriteLine("  ↓ - Runter");
-            Console.WriteLine("  → - Rechts");
-            Console.WriteLine();
-            Console.WriteLine("Spieler 2:");
-            Console.WriteLine("  W - Hoch");
-            Console.WriteLine("  A - Links");
-            Console.WriteLine("  S - Runter");
-            Console.WriteLine("  D - Rechts");
-            Console.WriteLine();
-            Console.WriteLine("Vermeide Kollisionen mit dir selbst oder dem Rand!");
-            Console.WriteLine("══════════════════════════════");
-            Console.WriteLine("Drücke eine beliebige Taste, um zum Menü zurückzukehren...");
-            Console.ReadKey();
-        }
-
-        // Statistik - Untermenü im Hauptmenü
-        static void Statistiken()
-        {
-            Console.Clear();
-            Console.WriteLine("Statistiken");
-            Console.WriteLine(" ");
-            Console.WriteLine("══════════════════════════════");
-            Console.WriteLine(" ");
-
-            int punktefürLevel = Program.xp % 100;
-
-            // Fortschrittsbalken
-            int balkenLänge = 20; // Balken 20 Zeichen lang
-            int gefüllteBlöcke = (punktefürLevel * balkenLänge) / 100;
-            string Fortschrittsbalken = new string('█', gefüllteBlöcke).PadRight(balkenLänge, '-');
-
-            Console.WriteLine($"Level:                    {Program.level}");
-            Console.WriteLine($"Fortschritt:              [{Fortschrittsbalken}] {punktefürLevel}/100");
-
-            Console.WriteLine(" ");
-            Console.WriteLine("══════════════════════════════");
-            Console.WriteLine($"Gesamte Spiele:           {Program.spieleGesamt}");
-            Console.WriteLine($"Höchste Punktzahl:        {Program.highscore}");
-            Console.WriteLine($"Durchschnittliche XP:     {(Program.spieleGesamt > 0 ? Program.xp / Program.spieleGesamt : 0)}");
-            Console.WriteLine($"Gesamte Coins:            {Program.gesamtcoins}");
-            Console.WriteLine($"Aktuelle Coins:           {Program.coins}");
-
-            Console.WriteLine("══════════════════════════════");
-            Console.WriteLine("Drücke eine beliebige Taste, um zum Menü zurückzukehren...");
-            Console.ReadKey();
         }
     }
 }
