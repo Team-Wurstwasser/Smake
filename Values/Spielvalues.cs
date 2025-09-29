ee736897c317322fb00f1c2a328c9c71bb1a9e86
hallo123wert <alexander.goetz11@web.de>
2025-09-29 15:50:23 +0200

    .

--- Values/Spielvalues.cs
+++ Values/Spielvalues.cs
@@ -4,18 +4,18 @@ namespace Smake.io.Values
 {
     public class Spielvalues
     {
-        public static int maxfutter;
+        public static int Maxfutter { get; set; }
 
         // Spielfeldgröße (Breite x Höhe)
         public readonly static int weite = GameData.Weite;
         public readonly static int hoehe = GameData.Hoehe;
 
         // Spielmodi
-        public static bool multiplayer;
-        public static string difficulty;
-        public static string gamemode;
+        public static bool Multiplayer { get; set; }
+        public static string? Difficulty { get; set; }
+        public static string? Gamemode { get; set; }
 
         // Spielgeschwindigkeit
-        public static int zeit;
+        public static int Zeit { get; set; }
     }
 }
