13604c8241e8674f6c976c213e7708fe897d102f
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2025-10-04 03:53:29 +0200

    fix

--- Helper/MusikSelector.cs
+++ Helper/MusikSelector.cs
@@ -1,69 +1,89 @@
 ﻿using Smake.Speicher;
-using Smake.Enums;
+using Smake.Values;
 
 namespace Smake.Helper
 {
-    public static class MusikSelector
+    public class MusikSelector
     {
-        public static readonly Dictionary<string, Dictionary<Difficulty, int>> MusikMap =
-            new()
+        public static int? Selector()
+        {
+            int? currentMusik = null;
+
+            switch (Spielvalues.Gamemode)
             {
-                {
-                    "Normal", new Dictionary<Difficulty, int>
-                    {   
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Normal.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Normal.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Normal.Schnell }
-                    }
-                },
-                {
-                    "Unendlich", new Dictionary<Difficulty, int>
+                case "Normal":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Unendlich.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Unendlich.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Unendlich.Schnell }
-                    }
-                },
-                {
-                    "Babymode", new Dictionary<Difficulty, int>
+                        "Langsam" => GameData.MusikDaten.Game.Normal.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Normal.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Normal.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Unendlich":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Babymode.Langsam },
-                        { Difficulty.Mittel,  GameData.MusikDaten.Game.Babymode.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Babymode.Schnell }
-                    }
-                },
-                {
-                    "Mauer-Modus", new Dictionary<Difficulty, int>
+                        "Langsam" => GameData.MusikDaten.Game.Unendlich.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Unendlich.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Unendlich.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Babymode":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Mauer_Modus.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Mauer_Modus.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Mauer_Modus.Schnell }
-                    }
-                },
-                {
-                    "Schlüssel-Modus", new Dictionary<Difficulty, int>
+                        "Langsam" => GameData.MusikDaten.Game.Babymode.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Babymode.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Babymode.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Mauer-Modus":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Schluessel_Modus.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Schluessel_Modus.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Schluessel_Modus.Schnell }
-                    }
-                },
-                {
-                    "Sprungfutter-Modus", new Dictionary<Difficulty, int>
+                        "Langsam" => GameData.MusikDaten.Game.Mauer_Modus.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Mauer_Modus.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Mauer_Modus.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Schlüssel-Modus":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Sprungfutter_Modus.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Sprungfutter_Modus.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Sprungfutter_Modus.Schnell }
-                    }
-                },
-                {
-                    "Chaos-Steuerung", new Dictionary<Difficulty, int>
+                        "Langsam" => GameData.MusikDaten.Game.Schluessel_Modus.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Schluessel_Modus.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Schluessel_Modus.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Sprungfutter-Modus":
+                    currentMusik = Spielvalues.Difficulty switch
                     {
-                        { Difficulty.Langsam, GameData.MusikDaten.Game.Chaos_Steuerung.Langsam },
-                        { Difficulty.Mittel, GameData.MusikDaten.Game.Chaos_Steuerung.Mittel },
-                        { Difficulty.Schnell, GameData.MusikDaten.Game.Chaos_Steuerung.Schnell }
-                    }
-                }
-            };
+                        "Langsam" => GameData.MusikDaten.Game.Sprungfutter_Modus.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Sprungfutter_Modus.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Sprungfutter_Modus.Schnell,
+                        _ => null
+                    };
+                    break;
+
+                case "Chaos-Steuerung":
+                    currentMusik = Spielvalues.Difficulty switch
+                    {
+                        "Langsam" => GameData.MusikDaten.Game.Chaos_Steuerung.Langsam,
+                        "Mittel" => GameData.MusikDaten.Game.Chaos_Steuerung.Mittel,
+                        "Schnell" => GameData.MusikDaten.Game.Chaos_Steuerung.Schnell,
+                        _ => null
+                    };
+                    break;
+            }
+
+            return currentMusik;
+        }
+
     }
 }
