9deabb9a37f684cca615133158eb45974dc057ad
hallo123wert <79359578+hallo123wert@users.noreply.github.com>
2026-02-03 01:47:12 +0100

    Revert "Auf NAudio umgestellt" (#21)

--- SFX/Sounds.cs
+++ SFX/Sounds.cs
@@ -1,79 +1,50 @@
-﻿using NAudio.Wave;
-using Smake.Speicher;
+﻿using Smake.Speicher;
+using System.Media;
 
 namespace Smake.SFX
 {
     public class Sounds
     {
-        private static bool _musikplay;
+        public static bool Musikplay { get; set; }
+        public static int Currentmusik { private get; set; }
+        public static bool Soundplay { get; set; }
 
-        public static bool Musikplay
-        {
-            get => _musikplay;
-            set
-            {
-                if (_musikplay == value)
-                {
-                    return;
-                }
-                _musikplay = value;
-
-                Melodie(lastmusik > -1 ? lastmusik.Value : 0);
-            }
-        }
-
-        private static bool _soundplay;
-
-        public static bool Soundplay
-        {
-            get => _soundplay;
-            set
-            {
-                if (_soundplay == value)
-                {
-                    return;
-                }
-                _soundplay = value;
-
-                Melodie(lastmusik > -1 ? lastmusik.Value : 0);
-            }
-        }
-
+        private static SoundPlayer? currentPlayer;
         private static int? lastmusik = -1;
         private static bool lastPlayState = false; // merkt sich, ob Musik an/aus war
-
-        private static WaveOutEvent? waveOutMusik;
-        private static AudioFileReader? audioFile;
-        private static LoopStream? loopStream;
 
         public static void Melodie(int Currentmusik)
         {
             if (!OperatingSystem.IsWindows()) return;
-            
+
             if (!Musikplay && !Soundplay)
             {
-                StopMusik();
+                currentPlayer?.Stop();
                 lastPlayState = false;
                 return;
             }
 
             if (Currentmusik != lastmusik || Musikplay != lastPlayState)
             {
-                StopMusik();
+                currentPlayer?.Stop();
+                currentPlayer = null;
 
                 string basePath = AppDomain.CurrentDomain.BaseDirectory;
+                string dateipfad;
 
-                string dateipfad = Musikplay
-                                    ? Path.Combine(basePath, "Sounds", GameData.Filenames[Currentmusik])
-                                    : Path.Combine(basePath, "Sounds", GameData.NoMusikFile);
+                if (Musikplay)
+                {
+                    dateipfad = Path.Combine(basePath, "Sounds", GameData.Filenames[Currentmusik]);
+                }
+                else
+                {
+                    dateipfad = Path.Combine(basePath, "Sounds", GameData.NoMusikFile);
+                }
 
                 if (File.Exists(dateipfad))
                 {
-                    waveOutMusik = new WaveOutEvent();
-                    audioFile = new AudioFileReader(dateipfad);
-                    loopStream = new LoopStream(audioFile);
-                    waveOutMusik.Init(loopStream);
-                    waveOutMusik.Play();
+                    currentPlayer = new SoundPlayer(dateipfad);
+                    currentPlayer.PlayLooping();
                 }
 
                 lastmusik = Currentmusik;
@@ -81,42 +52,11 @@ namespace Smake.SFX
             }
         }
 
-        private static void StopMusik()
-        {
-            if (waveOutMusik != null)
-            {
-                waveOutMusik.Stop();
-                waveOutMusik.Dispose();
-                waveOutMusik = null;
-            }
-
-            audioFile?.Dispose();
-            audioFile = null;
-
-            loopStream = null;
-        }
-
         public static void Playbeep()
         {
             if (!Soundplay || !OperatingSystem.IsWindows()) return;
 
-            string basePath = AppDomain.CurrentDomain.BaseDirectory;
-            string dateipfad = Path.Combine(basePath, "Sounds", GameData.BeepFile);
-
-            if (File.Exists(dateipfad))
-            {
-                var waveOutBeep = new WaveOutEvent();
-                var audioFile = new AudioFileReader(dateipfad);
-
-                waveOutBeep.Init(audioFile);
-                waveOutBeep.Play();
-
-                waveOutBeep.PlaybackStopped += (s, e) =>
-                {
-                    waveOutBeep.Dispose();
-                    audioFile.Dispose();
-                };
-            }
+            Console.Beep(700, 100);
         }
     }
 }
